1bfb5314ba69140b68de035ae4703bb5758fa8a3
jwa <jonas.wanner@clavisit.com>
2021-06-09 23:47:56 +0200

    change styling and add unstable message

--- src/new/mod.ts
+++ src/new/mod.ts
@@ -4,7 +4,7 @@ import Bundler from './Bundler'
 import Log from './Log'
 
 async function initialization () {
-  Log.write(Log.chalk.bgGreen('liferay-npm-bundler-improved'))
+  Log.write(Log.chalk.bgBlue('liferay-npm-bundler-improved'), Log.chalk.bgRed('unstable'))
   Log.write(Log.chalk.gray('this is a very experimental npm-bundler which takes a javascript portlet, bundles it with rollup and creates a jar file which then can be deployed to a liferay instance.'))
 
   const bundler: Bundler = new Bundler()
