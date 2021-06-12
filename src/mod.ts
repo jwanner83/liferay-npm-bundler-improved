7b49840a5953173c6af090351520e57d878856cc
Jonas Wanner <jonas@wanner.com>
2021-06-12 21:02:46 +0200

    update version in mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -7,7 +7,7 @@ import Log from './classes/Log'
 async function initialization () {
   const start: Date = new Date()
 
-  Log.write(Log.chalk.blue('liferay-npm-bundler-improved - 1.0.0-beta.0'))
+  Log.write(Log.chalk.blue('liferay-npm-bundler-improved - 1.0.0-beta.2'))
 
   const bundler: Bundler = new Bundler()
 
