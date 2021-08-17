2a99c3aad70a1d93deac3aea84cd1a8ded3e0cca
jwa <jonas.wanner@clavisit.com>
2021-08-17 19:01:59 +0200

    #4 Implementing the PackageHandler.ts class

--- src/handlers/PackageHandler.ts
+++ src/handlers/PackageHandler.ts
@@ -1,6 +1,37 @@
-// the package json of the portlet
+import Log from '../classes/Log'
+
 const pack = require(process.cwd() + '/package.json')
 
+/**
+ * The package handler which checks and serves the external package.json
+ */
 export default class PackageHandler {
+    /**
+     * The external package.json
+     * @private
+     */
     public static readonly pack = pack
+
+    /**
+     * The required fields which have to be present inside the external package.json
+     * @private
+     */
+    private static readonly necessaryFields = ['name', 'version', 'description']
+
+    /**
+     * Check if the package.json contains all the necessary fields
+     */
+    public static check () {
+        if (pack) {
+            for (const necessaryField of this.necessaryFields) {
+                if (!pack[necessaryField]) {
+                    Log.write(Log.chalk.red(`The package.json is missing the required field '${necessaryField}'.`))
+                    throw new Error()
+                }
+            }
+        } else {
+            Log.write(Log.chalk.red(`The package.json couldn't be read. Please check if it exists.`))
+            throw new Error()
+        }
+    }
 }
