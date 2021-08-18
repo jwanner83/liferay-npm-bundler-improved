a70ca775b7ece738783ef3261b8f59fc63e2d5da
jwa <jonas.wanner@clavisit.com>
2021-08-18 10:15:08 +0200

    #4 Reorder mod.ts and adding TimeHandler, Comment out methods which aren't currently implemented

--- src/mod.ts
+++ src/mod.ts
@@ -1,35 +1,34 @@
 #!/usr/bin/env node
+import TimeHandler from './handlers/TimeHandler'
 import yargs from 'yargs'
-import Bundler from './classes/Bundler'
 import Log from './classes/Log'
+import Bundler from './classes/Bundler'
+
+const arg: any = yargs(process.argv.slice(2)).options({
+  deploy: {
+    alias: 'd',
+    type: 'string'
+  },
+  keep: {
+    alias: 'k',
+    type: 'boolean'
+  }
+}).argv
 
 async function initialization () {
-  const start: Date = new Date()
-
-  Log.write(Log.chalk.blue('liferay-npm-bundler-improved - 1.0.0-beta.7'))
+  const timer = new TimeHandler()
+  Log.write(Log.chalk.bgBlue(' liferay-npm-bundler-improved - 1.0.0-beta.7 '))
 
   const bundler: Bundler = new Bundler()
 
-  const arg: any = yargs(process.argv.slice(2)).options({
-    deploy: {
-      alias: 'd',
-      type: 'string'
-    },
-    keep: {
-      alias: 'k',
-      type: 'boolean'
-    }
-  }).argv
-
   try {
-    Log.write('\npreparation')
-    await bundler.loadRollupConfiguration()
-    await bundler.createDistDirectory()
+    Log.write(Log.chalk.bgCyan('\n PREPARE '))
+    await bundler.prepare()
 
-    Log.write('\nbundle code with rollup')
+    Log.write(Log.chalk.bgCyan('\n BUNDLE '))
     await bundler.bundle()
 
-    Log.write('\nwrap liferay code into the Liferay.Loader')
+    /*Log.write('\nwrap liferay code into the Liferay.Loader')
     await bundler.wrap()
 
     Log.write('\ncheck additional portlet features')
@@ -46,11 +45,11 @@ async function initialization () {
     if (arg.deploy || arg.deploy === '') {
       Log.write('\nadditional: deploying')
       await bundler.deploy(arg.deploy)
-    }
+    }*/
 
-    Log.write(Log.chalk.bgGreen(`\nbundler finished successfully`), Log.chalk.bgBlack(`took ${(new Date().getTime() - start.getTime()) / 1000 }s`))
+    Log.write(Log.chalk.bgGreen('\n SUCCESS '), timer.getSecondsPretty(), Log.chalk.green(`bundler finished successfully`))
   } catch (exception) {
-    Log.write(Log.chalk.bgRed(`\nbundler finished with an error`), Log.chalk.bgBlack(`took ${(new Date().getTime() - start.getTime()) / 1000 }s`))
+    Log.write(Log.chalk.bgRed('\n ERROR '), timer.getSecondsPretty(), Log.chalk.red(`bundler finished with an error`))
   }
 }
 
