42e6cc32db22681ccf7a0dad4368c0215e1c9c16
jwa <jonas.wanner@clavisit.com>
2021-08-19 08:50:34 +0200

    #4 implement processing of all templates

--- src/classes/Bundler.ts
+++ src/classes/Bundler.ts
@@ -62,16 +62,15 @@ export default class Bundler {
     Log.write(Log.chalk.white('bundle code'))
     await this.rollupHandler.bundle()
 
-    Log.write(Log.chalk.white('write code to file'))
+    Log.write(Log.chalk.white('\nwrite code to file'))
     await this.rollupHandler.writeToFile()
-
-    Log.write(timer.getSecondsPretty(), Log.chalk.green(`finished rollup bundle tasks successfully`))
   }
 
   public async process () {
     const timer = new TimeHandler()
 
-    Log.write(Log.chalk.gray('wrap code in liferay loader'))
+    Log.write(Log.chalk.white('process templates and replace variables'))
+    Log.write(Log.chalk.gray('process wrapper.js'))
     const wrapperJsTemplate = new TemplateHandler('wrapper.js')
     wrapperJsTemplate.replace('name', this.packageHandler.pack.name)
     wrapperJsTemplate.replace('version', this.packageHandler.pack.version)
@@ -79,16 +78,31 @@ export default class Bundler {
     const bundle = await this.rollupHandler.getBundledCode()
     wrapperJsTemplate.replace('bundle', bundle)
 
+    Log.write(Log.chalk.gray('process MANIFEST.MF'))
+    const manifestMFTemplate = new TemplateHandler('MANIFEST.MF')
+    manifestMFTemplate.replace('name', this.packageHandler.pack.name)
+    manifestMFTemplate.replace('description', this.packageHandler.pack.description)
+    manifestMFTemplate.replace('version', this.packageHandler.pack.version)
+
+    Log.write(Log.chalk.gray('process manifest.json'))
+    const manifestJSONTemplate = new TemplateHandler('manifest.json')
+    manifestJSONTemplate.replace('name', this.packageHandler.pack.name)
+    manifestJSONTemplate.replace('version', this.packageHandler.pack.version)
+
+    Log.write(timer.getSecondsPretty(), Log.chalk.green(`finished template processing successful`))
+    timer.reset()
+
+    Log.write(Log.chalk.white('\nprocess jar file'))
+    Log.write(Log.chalk.gray('create file structure'))
     const meta = this.jarHandler.jar.folder('META-INF')
-    // meta.file('MANIFEST.MF', this.manifestMF)
+    meta.file('MANIFEST.MF', manifestMFTemplate.processed)
 
     const resources = meta.folder('resources')
-    // resources.file('manifest.json', this.manifestJSON)
+    resources.file('manifest.json', manifestJSONTemplate.processed)
+    resources.file(`${this.entryPoint}.js`, wrapperJsTemplate.processed)
     resources.file('package.json', JSON.stringify(this.packageHandler.pack))
-    resources.file(`${this.entryPoint}.js`, bundle)
 
-    await this.jarHandler.createJarFile()
-    Log.write(timer.getSecondsPretty(), Log.chalk.green('finished creating the jar file in the `dist` folder'))
+    Log.write(timer.getSecondsPretty(), Log.chalk.green('finished jar processing successful'))
   }
 
   /*public wrap = async () => {
