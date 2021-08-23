3141b051885cd1d6ec1c57fe79736acba9eb97e7
Jonas Wanner <jonas@wanner.com>
2021-08-23 22:41:06 +0200

    fix vue example

--- examples/vue/rollup.config.js
+++ examples/vue/rollup.config.js
@@ -1,19 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import vuePlugin from 'rollup-plugin-vue'
-import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.js',
   plugins: [
     resolve(),
     vuePlugin(),
-    commonjs(),
-    terser({
-      format: {
-        comments: false
-      }
-    })
+    commonjs()
   ],
   output: [
     {
