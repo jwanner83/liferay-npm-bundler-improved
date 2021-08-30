120c85c334cfac9d0cabaa4787dd0cf67a45b248
jwa <jonas.wanner@clavisit.com>
2021-08-30 15:27:58 +0200

    #4 vue example - add inject process env plugin to rollup config

--- examples/vue/rollup.config.js
+++ examples/vue/rollup.config.js
@@ -1,13 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import vuePlugin from 'rollup-plugin-vue'
+import injectProcessEnv from 'rollup-plugin-inject-process-env'
 
 export default {
   input: 'src/index.js',
   plugins: [
     resolve(),
     vuePlugin(),
-    commonjs()
+    commonjs(),
+    injectProcessEnv()
   ],
   output: [
     {
