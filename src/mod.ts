7d1a1804272f4424e4a37bc1f2750e6b0cab664e
jwa <jonas.wanner@clavisit.com>
2021-10-01 18:57:14 +0200

    #19 implement ProcessHandler into mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,19 @@
 import { name, version } from '../package.json'
+import ProcessHandlerImplementation from './implementations/ProcessHandlerImplementation'
+import ProcessHandler from './interfaces/ProcessHandler'
+import ExceptionHandler from './interfaces/ExceptionHandler'
+import ExceptionHandlerImplementation from './implementations/ExceptionHandlerImplementation'
 
 void (async () => {
+  const process: ProcessHandler = new ProcessHandlerImplementation(version)
+  const exceptionHandler: ExceptionHandler = new ExceptionHandlerImplementation()
+
   console.log(`${name} - ${version}`)
+
+  try {
+    await process.prepare()
+    await process.bundle()
+  } catch (exception) {
+    console.log('bundler failed:', exceptionHandler.getExceptionMessage(exception))
+  }
 })()
