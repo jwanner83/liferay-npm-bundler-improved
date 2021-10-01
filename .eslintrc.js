559d1d7d34580a53c753c790a063691faa198fd4
jwa <jonas.wanner@clavisit.com>
2021-10-01 18:54:08 +0200

    #19 configure .eslintrc.js and .prettierrc.js to work with each other

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
-  extends: [
-    'standard-with-typescript',
-    'prettier'
-  ],
+  extends: ['standard-with-typescript', 'prettier'],
   parserOptions: {
     project: './tsconfig.json'
   },
   rules: {
     '@typescript-eslint/strict-boolean-expressions': 'off',
-
+    'space-before-function-paren': 'off'
   }
 }
