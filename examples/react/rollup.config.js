0b8f236a3e7ee3ec767ed08e952ff716c8a62545
Jonas Wanner <jonas@wanner.com>
2021-11-29 18:46:18 +0100

    #19 add file to rollup config

--- examples/react/rollup.config.js
+++ examples/react/rollup.config.js
@@ -6,6 +6,7 @@ import replace from '@rollup/plugin-replace'
 export default {
   input: 'src/index.tsx',
   output: {
+    file: 'build/index.js',
     format: 'commonjs',
     exports: 'default'
   },
