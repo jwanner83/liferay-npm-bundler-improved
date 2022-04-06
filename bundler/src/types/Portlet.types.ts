e9faef73ef221c7446dea42de115fb152738c047
Jonas Wanner <jonas.wanner@clavisit.com>
2022-04-06 23:25:41 +0200

    #19 adding javax.portlet.resource-bundle string to npmbundlerrc portlet configuration type

--- bundler/src/types/Portlet.types.ts
+++ bundler/src/types/Portlet.types.ts
@@ -3,6 +3,11 @@ interface Portlet {
    * The name of the portlet
    */
   readonly 'javax.portlet.name': string
+
+  /**
+   * The location of the resource bundles
+   */
+  readonly 'javax.portlet.resource-bundle': string
 }
 
 export default Portlet
