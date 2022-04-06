8947140fdb1900489f6aea01820abfe8210c3af3
Jonas Wanner <jonas.wanner@clavisit.com>
2022-04-06 23:26:08 +0200

    #19 determine path to localization from npmbundlerrc file

--- bundler/src/handler/FeaturesHandler.ts
+++ bundler/src/handler/FeaturesHandler.ts
@@ -1,9 +1,35 @@
+import { readFile } from 'fs/promises'
+import Pack from '../types/Pack.types'
+import { sep } from 'path'
+import npmbundlerrc from '../types/npmbundlerrc.types'
+
 export default class FeaturesHandler {
+  public npmbundlerrc: npmbundlerrc
+  public localizationPath = `features${sep}localization`
   public hasLocalization = false
 
-  determine(pack: any): void {
+  async resolve(): Promise<void> {
+    try {
+      const file = await readFile('./.npmbundlerrc')
+      this.npmbundlerrc = await JSON.parse(file.toString())
+    } catch {
+      // npmbundlerrc could not be found. is not required
+    }
+  }
+
+  determine(pack: Pack): void {
     if (pack.portlet['javax.portlet.resource-bundle']) {
       this.hasLocalization = true
+
+      if (this.npmbundlerrc) {
+        const path = this.npmbundlerrc?.['create-jar']?.features?.localization
+
+        if (path) {
+          const split = path.split('/')
+          split.pop()
+          this.localizationPath = split.join('/')
+        }
+      }
     }
   }
 }
