635d419fb18a6ce4e37156312a92461bd0693837
Jonas Wanner <jonas.wanner@clavisit.com>
2022-04-26 18:35:56 +0200

    #9 include rollup-plugin-lnbi into react portlet

--- examples/react/rollup.config.js
+++ examples/react/rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import esbuild from 'rollup-plugin-esbuild'
 import replace from '@rollup/plugin-replace'
+import lnbi from 'rollup-plugin-lnbi'
 
 export default {
   input: 'src/index.tsx',
@@ -17,6 +18,7 @@ export default {
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    lnbi()
   ]
 }
