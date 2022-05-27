82c708d7caf84eadf942f645e1b8b85c75b679cb
Jonas Wanner <jonas.wanner@clavisit.com>
2022-05-27 16:32:51 +0200

    #39 add arg as dependency

--- bundler/rollup.config.ts
+++ bundler/rollup.config.ts
@@ -37,7 +37,7 @@ const config: RollupOptions = {
       banner: '#!/usr/bin/env node'
     }
   ],
-  external: ['fs', 'util', 'regenerator-runtime/runtime', 'path', 'archiver', 'chalk', 'readline']
+  external: ['fs', 'util', 'regenerator-runtime/runtime', 'path', 'archiver', 'chalk', 'readline', 'arg']
 }
 
 export default config
