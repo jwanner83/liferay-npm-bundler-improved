8c5acf48a1c71d3968673457fd1ef9a78e3e3a74
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-03 13:26:48 +0200

    #44 add property to package.json portlet type

--- bundler/src/types/Portlet.types.ts
+++ bundler/src/types/Portlet.types.ts
@@ -8,6 +8,11 @@ interface Portlet {
    * The location of the resource bundles
    */
   readonly 'javax.portlet.resource-bundle': string
+
+  /**
+   * The location of a css file which should be included in the page
+   */
+  readonly 'com.liferay.portlet.header-portlet-css'?: string
 }
 
 export default Portlet
