6ee6ae21ebac4a0a9bda7a1d6d1fefd140364d12
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-04 15:40:09 +0200

    #46 change from rollup to vite in vue example

--- examples/vue/src/components/index.vue
+++ examples/vue/src/components/index.vue
@@ -1,38 +1,36 @@
 <template>
-  <div>
+  <div class='vue'>
     <div>
-      <div>
-        <span class="tag">
-          {{ label.portletNamespace }}:
-        </span>
-        <span class="value">
-          {{ $root.$data.portletNamespace }}
-        </span>
-      </div>
-      <div>
-        <span class="tag">
-          {{ label.contextPath }}:
-        </span>
-        <span class="value">
-          {{ $root.$data.contextPath }}
-        </span>
-      </div>
-      <div>
-        <span class="tag">
-          {{ label.portletElementId }}:
-        </span>
-        <span class="value">
-          {{ $root.$data.portletElementId }}
-        </span>
-      </div>
-      <div>
-        <span class="tag">
-          {{ label.configuration }}:
-        </span>
-        <span class="value pre">
-          {{ JSON.stringify($root.$data.configuration, null, 2) }}
-        </span>
-      </div>
+      <span class="tag">
+        {{ label.portletNamespace }}:
+      </span>
+      <span class="value">
+        {{ $root.$data.portletNamespace }}
+      </span>
+    </div>
+    <div>
+      <span class="tag">
+        {{ label.contextPath }}:
+      </span>
+      <span class="value">
+        {{ $root.$data.contextPath }}
+      </span>
+    </div>
+    <div>
+      <span class="tag">
+        {{ label.portletElementId }}:
+      </span>
+      <span class="value">
+        {{ $root.$data.portletElementId }}
+      </span>
+    </div>
+    <div>
+      <span class="tag">
+        {{ label.configuration }}:
+      </span>
+      <span class="value pre">
+        {{ JSON.stringify($root.$data.configuration, null, 2) }}
+      </span>
     </div>
   </div>
 </template>
@@ -57,3 +55,16 @@ export default Vue.extend({
   }
 })
 </script>
+
+<style lang='scss' scoped>
+.vue {
+  .tag {
+    font-weight: bold;
+    font-family: monospace;
+  }
+
+  .value {
+    font-family: monospace;
+  }
+}
+</style>
