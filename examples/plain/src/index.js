92c40371605b8fb06d2c4f690e7b219e35fc9952
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-04 15:39:19 +0200

    #46 adding wrapper around plain example

--- examples/plain/src/index.js
+++ examples/plain/src/index.js
@@ -1,47 +1,45 @@
 /**
- * This is the main entry point of the portlet.
+ * Main portlet entrypoint for Liferay.
  *
- * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent 
- * information on the signature of this function.
- *
- * @param  {Object} params a hash with values of interest to the portlet
- * @return {void}
+ * @param params
  */
 function main(params) {
     const node = document.getElementById(params.portletElementId);
 
     node.innerHTML =
-        '<div>' +
-            '<span class="tag">' +
-                Liferay.Language.get('portlet-namespace') + ': ' +
-            '</span>' +
-            '<span class="value">' +
-                params.portletNamespace +
-            '</span>' +
-        '</div>' +
-        '<div>' +
-            '<span class="tag">' +
-                Liferay.Language.get('context-path') + ': ' +
-            '</span>' +
-            '<span class="value">' +
-                params.contextPath +
-            '</span>' +
-        '</div>' +
-        '<div>' +
-            '<span class="tag">' +
-                Liferay.Language.get('portlet-element-id') + ': ' +
-            '</span>' +
-            '<span class="value">' +
-                params.portletElementId +
-            '</span>' +
-        '</div>' +
-        '<div>' +
-            '<span class="tag">' +
-                Liferay.Language.get('configuration') + ': ' +
-            '</span>' +
-            '<span class="value">' +
-                JSON.stringify(params.configuration, null, 2) +
-            '</span>' +
+        '<div class="plain">' +
+            '<div>' +
+                '<span class="tag">' +
+                    Liferay.Language.get('portlet-namespace') + ': ' +
+                '</span>' +
+                '<span class="value">' +
+                    params.portletNamespace +
+                '</span>' +
+            '</div>' +
+            '<div>' +
+                '<span class="tag">' +
+                    Liferay.Language.get('context-path') + ': ' +
+                '</span>' +
+                '<span class="value">' +
+                    params.contextPath +
+                '</span>' +
+            '</div>' +
+            '<div>' +
+                '<span class="tag">' +
+                    Liferay.Language.get('portlet-element-id') + ': ' +
+                '</span>' +
+                '<span class="value">' +
+                    params.portletElementId +
+                '</span>' +
+            '</div>' +
+            '<div>' +
+                '<span class="tag">' +
+                    Liferay.Language.get('configuration') + ': ' +
+                '</span>' +
+                '<span class="value">' +
+                    JSON.stringify(params.configuration, null, 2) +
+                '</span>' +
+            '</div>' +
         '</div>'
     ;
 }
