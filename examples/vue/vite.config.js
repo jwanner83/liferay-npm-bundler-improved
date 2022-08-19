e7ef2149c14b674193a960d1f37fa855c7a980d0
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-19 09:50:28 +0200

    #49 remove rollup options and move replace to vite plugin list

--- examples/vue/vite.config.js
+++ examples/vue/vite.config.js
@@ -1,27 +1,17 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import esbuild from 'rollup-plugin-esbuild'
 import replace from '@rollup/plugin-replace'
 import vue from '@vitejs/plugin-vue2'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      'process.env.BUILD': JSON.stringify('web')
+    })
   ],
   build: {
-    rollupOptions: {
-      plugins: [
-        resolve(),
-        commonjs(),
-        esbuild(),
-        replace({
-          preventAssignment: true,
-          'process.env.NODE_ENV': JSON.stringify('production'),
-          'process.env.BUILD': JSON.stringify('web')
-        })
-      ]
-    },
     outDir: 'build',
     lib: {
       entry: 'src/index.ts',
