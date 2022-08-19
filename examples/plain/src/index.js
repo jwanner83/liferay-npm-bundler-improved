a2d6ade0f53de90fac1c9486b6d7e9c094fd1901
Jonas Wanner <jonas.wanner@clavisit.com>
2022-08-19 09:43:02 +0200

    #49 format index.js

--- examples/plain/src/index.js
+++ examples/plain/src/index.js
@@ -4,44 +4,47 @@
  * @param params
  */
 function main(params) {
-    const node = document.getElementById(params.portletElementId);
+  const node = document.getElementById(params.portletElementId)
 
-    node.innerHTML =
-        '<div class="plain">' +
-            '<div>' +
-                '<span class="tag">' +
-                    Liferay.Language.get('portlet-namespace') + ': ' +
-                '</span>' +
-                '<span class="value">' +
-                    params.portletNamespace +
-                '</span>' +
-            '</div>' +
-            '<div>' +
-                '<span class="tag">' +
-                    Liferay.Language.get('context-path') + ': ' +
-                '</span>' +
-                '<span class="value">' +
-                    params.contextPath +
-                '</span>' +
-            '</div>' +
-            '<div>' +
-                '<span class="tag">' +
-                    Liferay.Language.get('portlet-element-id') + ': ' +
-                '</span>' +
-                '<span class="value">' +
-                    params.portletElementId +
-                '</span>' +
-            '</div>' +
-            '<div>' +
-                '<span class="tag">' +
-                    Liferay.Language.get('configuration') + ': ' +
-                '</span>' +
-                '<span class="value">' +
-                    JSON.stringify(params.configuration, null, 2) +
-                '</span>' +
-            '</div>' +
-        '</div>'
-    ;
+  node.innerHTML =
+    '<div class="plain">' +
+    '<div>' +
+    '<span class="tag">' +
+    Liferay.Language.get('portlet-namespace') +
+    ': ' +
+    '</span>' +
+    '<span class="value">' +
+    params.portletNamespace +
+    '</span>' +
+    '</div>' +
+    '<div>' +
+    '<span class="tag">' +
+    Liferay.Language.get('context-path') +
+    ': ' +
+    '</span>' +
+    '<span class="value">' +
+    params.contextPath +
+    '</span>' +
+    '</div>' +
+    '<div>' +
+    '<span class="tag">' +
+    Liferay.Language.get('portlet-element-id') +
+    ': ' +
+    '</span>' +
+    '<span class="value">' +
+    params.portletElementId +
+    '</span>' +
+    '</div>' +
+    '<div>' +
+    '<span class="tag">' +
+    Liferay.Language.get('configuration') +
+    ': ' +
+    '</span>' +
+    '<span class="value">' +
+    JSON.stringify(params.configuration, null, 2) +
+    '</span>' +
+    '</div>' +
+    '</div>'
 }
 
-module.exports = main;
+module.exports = main
